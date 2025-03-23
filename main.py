b9cf6154b4d5aaa1565d72771116e014acbe5426
M Nithin <mnithinsavi@gmail.com>
2025-03-23 10:27:46 +0530

    Updated version

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+"""Py game mail file"""
+
 import GameEnv
 import pygame
 import numpy as np
@@ -5,44 +7,56 @@ from ddqn_keras import DDQNAgent
 from collections import deque
 import random, math
 
-TOTAL_GAMETIME = 1000 # Max game time for one episode
-N_EPISODES = 10000
-REPLACE_TARGET = 50 
+TOTAL_GAMETIME = 1000  # Max game time for one episode
+N_EPISODES = 1000
+REPLACE_TARGET = 50
 
 game = GameEnv.RacingEnv()
 game.fps = 60
 
-GameTime = 0 
+GameTime = 0
 GameHistory = []
 renderFlag = False
 
-ddqn_agent = DDQNAgent(alpha=0.0005, gamma=0.99, n_actions=5, epsilon=1.00, epsilon_end=0.10, epsilon_dec=0.9995, replace_target= REPLACE_TARGET, batch_size=512, input_dims=19)
+ddqn_agent = DDQNAgent(
+    alpha=0.0005,
+    gamma=0.99,
+    n_actions=5,
+    epsilon=1.00,
+    epsilon_end=0.10,
+    epsilon_dec=0.9995,
+    replace_target=REPLACE_TARGET,
+    batch_size=512,
+    input_dims=19,
+)
 
 # if you want to load the existing model uncomment this line.
 # careful an existing model might be overwritten
-#ddqn_agent.load_model()
+# ddqn_agent.load_model()
 
 ddqn_scores = []
 eps_history = []
 
+
 def run():
+    """Reinforcment learning"""
 
     for e in range(N_EPISODES):
-        
-        game.reset() #reset env 
+
+        game.reset()  # reset env
 
         done = False
         score = 0
         counter = 0
-        
+
         observation_, reward, done = game.step(0)
         observation = np.array(observation_)
 
-        gtime = 0 # set game time back to 0
-        
-        renderFlag = True # if you want to render every episode set to true
+        gtime = 0  # set game time back to 0
 
-        if e % 10 == 0 and e > 0: # render every 10 episodes
+        renderFlag = True  # if you want to render every episode set to true
+
+        if e % 10 == 0 and e > 0:  # render every 10 episodes
             renderFlag = True
 
         while not done:
@@ -71,7 +85,7 @@ def run():
             ddqn_agent.remember(observation, action, reward, observation_, int(done))
             observation = observation_
             ddqn_agent.learn()
-            
+
             gtime += 1
 
             if gtime >= TOTAL_GAMETIME:
@@ -82,7 +96,7 @@ def run():
 
         eps_history.append(ddqn_agent.epsilon)
         ddqn_scores.append(score)
-        avg_score = np.mean(ddqn_scores[max(0, e-100):(e+1)])
+        avg_score = np.mean(ddqn_scores[max(0, e - 100) : (e + 1)])
 
         if e % REPLACE_TARGET == 0 and e > REPLACE_TARGET:
             ddqn_agent.update_network_parameters()
@@ -90,10 +104,17 @@ def run():
         if e % 10 == 0 and e > 10:
             ddqn_agent.save_model()
             print("save model")
-            
-        print('episode: ', e,'score: %.2f' % score,
-              ' average score %.2f' % avg_score,
-              ' epsolon: ', ddqn_agent.epsilon,
-              ' memory size', ddqn_agent.memory.mem_cntr % ddqn_agent.memory.mem_size)   
 
-run()        
+        print(
+            "episode: ",
+            e,
+            "score: %.2f" % score,
+            " average score %.2f" % avg_score,
+            " epsolon: ",
+            ddqn_agent.epsilon,
+            " memory size",
+            ddqn_agent.memory.mem_cntr % ddqn_agent.memory.mem_size,
+        )
+
+
+run()
